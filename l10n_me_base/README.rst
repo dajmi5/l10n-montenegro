fe07516c2381bcc528b89d7ad85ffd40dcc22c0a
Davor Bojkić <bole@dajmi5.com>
2018-06-06 10:12:13 +0200

    base montenegro data

---
+++ l10n_me_base/README.rst
@@ -0,0 +1,18 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+Montenegro : Cities and Banks
+=============================
+
+Postal codes for cities in Montenegro
+
+(https://github.com/OCA/partner-contact/tree/10.0/base_location)
+
+
+
+Contributors
+------------
+
+Davor Bojkić <bole@dajmi5.com>
+
---
+++ l10n_me_base/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "l10n_me_base",
+    "summary": "Montenegro cities and banks",
+    "category": "Localisation",
+    "images": [],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "MONTEX EL d.o.o.",
+    "licence": "LGPL-3",
+    "depends": [
+        "base_location",
+    ],
+    "data": [
+        "data/res.better.zip.csv",
+        "data/res.bank.csv"
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "auto_install": False,
+    "installable": True,
+}
+
